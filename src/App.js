95035972ccfadf1226d87aca63c5137124ccccdb
Pritam Pawar <pritam.pawar@Yash.com>
2022-09-28 18:19:31 +0530

    adding navbar and video design

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+
+import { BrowserRouter as Router} from 'react-router-dom';
+
+import HeroSection from './components/HeroSection';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <HeroSection></HeroSection>
+      </Router>
+    </>
+  );
+}
+
+export default App;
