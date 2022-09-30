beb1556c455aa73247f43ada9a8ef29cc63e6238
Pritam Pawar <pritam.pawar@Yash.com>
2022-09-30 19:22:20 +0530

    adding travel webpage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
-
-import { BrowserRouter as Router} from 'react-router-dom';
-
-import HeroSection from './components/HeroSection';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-        <HeroSection></HeroSection>
+        <Routes>
+          <Route path='/' exact element={<Home/>} />
+          <Route path='/services' element={<Services/>} />
+          <Route path='/products' element={<Products/>} />
+          <Route path='/sign-up' element={<SignUp/>} />
+        </Routes>
       </Router>
     </>
   );
